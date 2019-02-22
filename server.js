16a9b8c15202c55d30ca97b8c37a5cb81bacfb31
Enoka Jaona <Ejaona@gmail.com>
2019-02-22 11:25:41 -0600

    First get query completed

--- server.js
+++ server.js
@@ -9,4 +9,13 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.get("/projects", async (req, res) => {
+  try {
+    const projects = await db("projects");
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = server;
